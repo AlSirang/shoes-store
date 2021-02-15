9c6240426acc73c0d583aeab2726deb4c17deb10
bashir uddin <uddinbashir1999@gmail.com>
2021-02-16 01:32:55 +0500

    add dummy data for app

---
+++ src/data.js
@@ -0,0 +1,39 @@
+// dummy data
+export const shoes = {
+    jordan: {
+        imgSrc:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9937915b-34f8-458f-ab8a-c9c1e4ce428c/jordan-why-not-zer04-basketball-shoe-QWssLZ.jpg",
+        description: "Basketball Shoe",
+        price: "$120",
+    },
+    "nike-air-max-plus": {
+        imgSrc:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a4528acb-5813-4719-860d-4885b6e726a1/air-max-plus-mens-shoe-0RmbKC.jpg",
+        description: "Men's Shoe",
+        price: "$160",
+    },
+    "nike-fontanka-edge": {
+        imgSrc:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/717e1369-4ca7-4a4a-9c13-5455e679efba/fontanka-edge-womens-shoe-pZ16rB.jpg",
+        description: "Women's Shoe",
+        price: "$140",
+    },
+    "nike-air-max-90": {
+        imgSrc:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f632dcae-2a3e-485b-83c2-205011a7963a/air-max-90-little-kids-shoe-1DZRJ1.jpg",
+        description: "Little Kids' Shoe",
+        pirce: "$80",
+    },
+    "Nike Blazer Mid '77 Big Kids' Shoe": {
+        imgSrc:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/7b6dc415-6b81-4479-9171-229932972b76/blazer-mid-77-big-kids-shoe-4VfSTd.jpg",
+        description: "Big Kids' Shoe",
+        pirce: "$85",
+    },
+    "Nike Blazer Mid '77 SE Women's Shoe": {
+        imgSrc:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/77f3c27d-bb4b-42b6-88db-62ae9a16ac19/blazer-mid-77-se-womens-shoe-F2jmJF.jpg",
+        description: "Big Kids' Shoe",
+        pirce: "$105",
+    },
+};
